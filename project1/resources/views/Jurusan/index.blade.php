56449a61ece50f3a2760a5b9446ee2971a344394
AdjieAdrianSaputra <adjieadrian12@gmail.com>
2024-05-16 12:22:58 +0700

    proses edit dan hapus
    termasuk tombol dan modal untuk hapus

--- project1/resources/views/Jurusan/index.blade.php
+++ project1/resources/views/Jurusan/index.blade.php
@@ -44,8 +44,36 @@
                         <td>{{$item->kode}}</td>
                         <td>{{$item->jurusan}}</td>
                         <td>
+                            <!-- Tombol edit -->
                             <a href="/jurusan/edit/{{$item->id}}" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></a>
-                            <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+
+                            <!-- Tombol Hapus -->
+                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#hapus{{$item->id}}">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                            <!-- Modal -->
+                            <div class="modal fade" id="hapus{{$item->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Peringatan</h1>
+                                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Yakin Data Jurusan {{$item->jurusan}} di Hapus?
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                    <form action="/jurusan/{{$item->id}}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">Hapus</button>
+                                    </form>
+
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
                 @empty
